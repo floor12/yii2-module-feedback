3ea1ba0f3ffe7f8c1a5058e4d3b75bf3a9e655b4
floor12 <floor12@floor12.net>
2019-04-10 19:18:03 +0200

    Init commit

---
+++ src/Module.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace floor12\feedback;
+
+use Yii;
+/**
+ * feedback module definition class
+ * @property  string $editRole
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @var string
+     */
+    public $adminlayout = '@app/views/layouts/main';
+
+    /**
+     * @var string
+     */
+    public $viewForm= '@vendor/floor12/yii2-module-feedback/src/views/frontend/_form';
+
+    /**
+     * @var string
+     */
+    public $viewResult= '@vendor/floor12/yii2-module-feedback/src/views/frontend/_result';
+
+    /**
+     * @var string
+     */
+    public $userModel = 'app\models\User';
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'floor12\feedback\controllers';
+
+    /**
+     * @var string
+     */
+    public $adminRoles = ['@'];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->registerTranslations();
+    }
+
+
+    /**
+     *
+     */
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['app.f12.feedback'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@vendor/floor12/yii2-module-feedback/src/messages',
+            'sourceLanguage' => 'en-US',
+            'fileMap' => [
+                'app.f12.feedback' => 'feedback.php',
+            ],
+        ];
+    }
+
+}
