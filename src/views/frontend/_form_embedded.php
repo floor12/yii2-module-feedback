0f27ed753cf69467b1367e3568a971ade954ba5e
floor12 <floor12@floor12.net>
2025-02-10 11:16:12 +0100

    Autofocus var added to template.

--- src/views/frontend/_form_embedded.php
+++ src/views/frontend/_form_embedded.php
@@ -2,6 +2,7 @@
 /**
  * @var $this View
  * @var $model Feedback
+ * @var $autofocus bool
  */
 
 use floor12\feedback\models\Feedback;
@@ -23,7 +24,7 @@ $form = ActiveForm::begin(['id' => 'feedback-form']);
 
 <div class="row">
     <div class="col-md-4">
-        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'name')->textInput(['autofocus' => $autofocus ?? true]) ?>
     </div>
     <div class="col-md-4">
         <?= $form->field($model, 'phone')->textInput() ?>
